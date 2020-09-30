cce7f06c487b8edaca86a82afccdf8df1dcbddd3
lheokk <lheokk@gmail.com>
2020-09-30 15:15:01 +0200

    2

--- tpl/main_template.php
+++ tpl/main_template.php
@@ -1,259 +1,232 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=<?= $g_config['charset']?>" />
+        <meta http-equiv="Content-Type" content="text/html; charset=<?= $g_config['charset'] ?>" />
 
-        <title><?= $g_title?></title>
-        <?php if ( ! empty($g_description)):?>
-            <meta name="description" content="<?= $g_description?>" />
-        <?php endif;?>
-        <?php if ( ! empty($g_keywords)):?>
-            <meta name="keywords" content="<?= $g_keywords?>" />
-        <?php endif;?>
+        <title><?= $g_title ?></title>
+        <?php if (!empty($g_description)): ?>
+            <meta name="description" content="<?= $g_description ?>" />
+        <?php endif; ?>
+        <?php if (!empty($g_keywords)): ?>
+            <meta name="keywords" content="<?= $g_keywords ?>" />
+        <?php endif; ?>
 
-        <link rel="icon" href="<?= Root('favicon.ico')?>" type="image/x-icon" />
-        <link rel="shortcut icon" href="<?= Root('favicon.ico')?>" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/normalize.css')?>" />
-        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/dev/funcs.css')?>" />
-        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/main.css')?>" />
+        <link rel="icon" href="<?= Root('favicon.ico') ?>" type="image/x-icon" />
+        <link rel="shortcut icon" href="<?= Root('favicon.ico') ?>" type="image/x-icon" />
+
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/normalize.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/dev/funcs.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/main.css') ?>" />
 
         <script type="text/javascript" src="<?= Root('i/js/jquery-1.7.2.min.js') ?>"></script>
         <script type="text/javascript" src="<?= Root('i/js/main.js') ?>"></script>
         <!-- extraPacker -->
     </head>
     <body>
-        <?php header('Content-type: text/html; charset=utf-8');  ?>
-<!doctype html>
-<html lang="ru">
-  <head>
-      
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="styyle.css"> 
-    <title>health</title>
-  </head>
-  
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="styyle.css"> 
+        <title>health</title>
+    </head>
+
 <body>
     <style> .wrap {width: 100%; margin: 0 auto}</style>
     <style> .full {width: 100%; height: auto}</style>
-    
-          
-    
+    <div class="row">
+        <div class="col">
+            <div class="menu-container">
+                <div class="menu star">
+                    <div class= ' rusmed'> РУСМЕДЗДРАВ
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        <div class="col">
+            <div class="menu-container ">
+                <div class="menu">
+                    <ul>
+                        <li>О центре</li>
+                        <li>Услуги</li>
+                        <li>Акции</li>
+                        <li>Партнёрство</li>
+                        <!-- What ever, basic html menu(lists)! Dont worry megamenu.js will know -->
+                    </ul>
+                </div>
+            </div>
+        </div> 
+
+        <div class="col">
+            <div class="menu-container ">
+                <div class="menu ">
+                    <ul>
+                        <li>Карьера</li>
+                        <li>Блог</li>
+                        <!-- What ever, basic html menu(lists)! Dont worry megamenu.js will know -->
+                    </ul>
+                </div>
+            </div>
+        </div> 
+    </div>
+
+    <div class='full'>     
+        <div class="container ">
             <div class="row">
                 <div class="col">
                     <div class="menu-container">
-                        <div class="menu star">
-                            <div class= ' rusmed'> РУСМЕДЗДРАВ
+                        <div class="menu">
+                            <div class='blue-line full'> COVID-19. Тестирование на коронавирус с выездом на дом и предприятие. Читать подробнее
                             </div>
                         </div>
                     </div>
-                </div> 
-                
-                <div class="col">
-                    <div class="menu-container ">
-                        <div class="menu">
-                            <ul>
-                                <li>О центре</li>
-                                <li>Услуги</li>
-                                <li>Акции</li>
-                                <li>Партнёрство</li>
-                          <!-- What ever, basic html menu(lists)! Dont worry megamenu.js will know -->
-                            </ul>
-                        </div>
-                    </div>
-                </div> 
-                
-                <div class="col">
-                    <div class="menu-container ">
-                        <div class="menu ">
-                            <ul>
-                                <li>Карьера</li>
-                                <li>Блог</li>
-                  <!-- What ever, basic html menu(lists)! Dont worry megamenu.js will know -->
-                            </ul>
-                        </div>
-                    </div>
-                </div> 
+                </div>
             </div>
-    
-        <div class='full'>     
-            <div class="container ">
+        </div>
+    </div>
+    <div class='row'>
+        <div class='col med'>
+
+        </div>    
+    </div>
+    <div class='container'>    
+        <div class='row'>
+            <div class='col offset-1'>
+                <div class='bell'>
+                    Заказать обратный звонок
+                </div>
+            </div>
+        </div>
+
+
+        <div class='row '>
+            <div class='col offset-7'>
+
+                ООО «РУСМЕДЗДРАВ» предоставляет полный <br> пакет услуг согласно Приказу Министерства <br> здравоохранения и социального развития РФ <br> от 12.04.2011№ 302 н «Об утверждении перечней <br> вредных и (или) опасных производственных <br> факторов, при выполнении которых проводятся <br> обязательные предварительные и периодические <br> осмотры (обследования)»
+
+            </div>
+        </div>
+
+        <div class='bg-F7'>
+            <div class="container">
                 <div class="row">
                     <div class="col">
-                        <div class="menu-container">
-                            <div class="menu">
-                                <div class='blue-line full'> COVID-19. Тестирование на коронавирус с выездом на дом и предприятие. Читать подробнее
-                                </div>
+                        <div class='book-img'></div>
+                        <div class="low"> Дешевле, чем у конкурентов</div>
+                    </div>
+                    <div class="col">
+
+                        <div class="line">
+                            <div class="kniga-img line">
+                                <div class="med-book line"<br>Медицинские книжки </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-                <div class='row'>
-                    <div class='col med'>
-                       
-                    </div>    
-                </div>
-            <div class='container'>    
-                <div class='row'>
-                    <div class='col offset-1'>
-                        <div class='bell'>
-                            Заказать обратный звонок
-                        </div>
-                    </div>
-               </div>
-           
-           
-               <div class='row '>
-                   <div class='col offset-7'>
-                       
-                       ООО «РУСМЕДЗДРАВ» предоставляет полный <br> пакет услуг согласно Приказу Министерства <br> здравоохранения и социального развития РФ <br> от 12.04.2011№ 302 н «Об утверждении перечней <br> вредных и (или) опасных производственных <br> факторов, при выполнении которых проводятся <br> обязательные предварительные и периодические <br> осмотры (обследования)»
-                    
-               </div>
-            </div>
-                
-    <div class='bg-F7'>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class='book-img'></div>
-                    <div class="low"> Дешевле, чем у конкурентов</div>
-                </div>
-                <div class="col">
-                  
-                        <div class="line">
-                    <div class="kniga-img line">
-                    <div class="med-book line"<br>Медицинские книжки </div>
-                        </div>
-                   
-                    <div class="both"></div>
-                    <div class="garant">
-                        У нас вы можете заказать оформление <br> и продление Медицинской книжки. Мы <br> гарантируем, что....
-                    </div>
-                    <div class="read"> Читать полностью...</div>
-                </div>
-            </div> 
-        </div>
-        
-        <div class="container">
-            <div class="row">
-                  <div class="col">
-                  
-                        <div class="line">
-                    <div class="list-img line">
-                    <div class="med-book line"<br>Медицинские справки </div>
-                        </div>
-                   
-                    <div class="both"></div>
-                    <div class="garant">
-                        Мы обладаем медицинской лицензией МинЗдрава РФ и предоставляет только официальные документы...
-                    </div>
-                    <div class="read"> Читать полностью...</div>
-                </div>
-            </div> 
-                <div class="col">
-                    <div class=""></div>
-                    <div class="doctor-img"></div>
-                    <div class="low"> Дешевле, чем у конкурентов</div>
-                
-            </div>
-        </div>
 
-        </div>
-            </div>
-        <div class="star2"></div>
-        </div>
-    
-   <div class="container">
-       <div class="row">
-         <div class="col">
-            <div class="success">Наш успех заключается не только в <br> качестве и в максимальном комфорте для <br> организаций и частных лиц. В первую <br> очень - это приятные цены.</div>
-            <div class="prices"> Развернуть цены на услуги </div>
-        </div> 
-         </div> 
-   </div> 
-                
-    <div class="bg-v">
-        <div class="container">
-            <div class="row">
-            <div class="col">
-                <div class="online"> Оставить онлайн заявку </div>
-                <div class="free">Чтобы получить бесплатную консультацию <br> физическому или юридическому лицу, <br> необходимо заполнить форму или обратиться <br>к нашим специалистам по телефону <br>
-+ 7 (495) 666-0-00. </div>
-            </div>
-            
-            
-            <div class="col">
-                
-                <div class="form form-1">
-                <input type="text" value="" name="first_name"  placeholder="Имя" class="form-input" />
+                            <div class="both"></div>
+                            <div class="garant">
+                                У нас вы можете заказать оформление <br> и продление Медицинской книжки. Мы <br> гарантируем, что....
+                            </div>
+                            <div class="read"> Читать полностью...</div>
+                        </div>
+                    </div> 
                 </div>
-                
-                <div class="form ">
-                    <input type="text" value="" name="second_name" placeholder="Фамилия" class="form-input" />
-                </div>
-                
-                <div class="form">
-                <input type="text" value="" name="email"  placeholder="Номер телефона" class="form-input" />
-                </div>
-                
-                <div class="form ">
-                <input type="text" value="" name="title"  placeholder="Заголовок" class="form-input" />
-                </div>
-            
-                <div class="form">
-                <input type="text" value="" name="Company"  placeholder="Компания" class="form-input" />
-                </div>
-                
-               
 
-            </div>
-           </div>
-        </div>
-        <div class='up'></div>
-    </div>
                 <div class="container">
                     <div class="row">
                         <div class="col">
-                            <div class="sales">
-                            Для организаций предусмотрена гибкая система скидок
+
+                            <div class="line">
+                                <div class="list-img line">
+                                    <div class="med-book line"<br>Медицинские справки </div>
+                                </div>
+
+                                <div class="both"></div>
+                                <div class="garant">
+                                    Мы обладаем медицинской лицензией МинЗдрава РФ и предоставляет только официальные документы...
+                                </div>
+                                <div class="read"> Читать полностью...</div>
                             </div>
-                        </div>                    
-                    
+                        </div> 
                         <div class="col">
-                            <div class="want">Хотите сэкономить до 30%? Получить<br> надежного партнера,  избавиться от <br>головных болей и всегда знать о том, что<br> ваши сотрудники будут иметь<br> необходимые документы на руках?</div>
-                             <div class="offer">Читать индивидуальное предложение</div>
+                            <div class=""></div>
+                            <div class="doctor-img"></div>
+                            <div class="low"> Дешевле, чем у конкурентов</div>
+
                         </div>
-                        
-                        
-                            
-                       
+                    </div>
+
+                </div>
+            </div>
+            <div class="star2"></div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="success">Наш успех заключается не только в <br> качестве и в максимальном комфорте для <br> организаций и частных лиц. В первую <br> очень - это приятные цены.</div>
+                    <div class="prices"> Развернуть цены на услуги </div>
+                </div> 
+            </div> 
+        </div> 
+
+        <div class="bg-v">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="online"> Оставить онлайн заявку </div>
+                        <div class="free">Чтобы получить бесплатную консультацию <br> физическому или юридическому лицу, <br> необходимо заполнить форму или обратиться <br>к нашим специалистам по телефону <br>
+                            + 7 (495) 666-0-00. </div>
+                    </div>
+
+
+                    <div class="col">
+
+                        <div class="form form-1">
+                            <input type="text" value="" name="first_name"  placeholder="Имя" class="form-input" />
+                        </div>
+
+                        <div class="form ">
+                            <input type="text" value="" name="second_name" placeholder="Фамилия" class="form-input" />
+                        </div>
+
+                        <div class="form">
+                            <input type="text" value="" name="email"  placeholder="Номер телефона" class="form-input" />
+                        </div>
+
+                        <div class="form ">
+                            <input type="text" value="" name="title"  placeholder="Заголовок" class="form-input" />
+                        </div>
+
+                        <div class="form">
+                            <input type="text" value="" name="Company"  placeholder="Компания" class="form-input" />
+                        </div>
+
+
+
                     </div>
                 </div>
-</body>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="js/megamenu.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-</html>
-        <div class="wrap">
-            <?php IncludeCom('tpl_coms/head')?>
-            <!--контент-->
-            <div id="content">
-                <?= $content?>
             </div>
-            <!--контент закончился-->
-            <?php IncludeCom('tpl_coms/footer')?>
+            <div class='up'></div>
         </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="sales">
+                        Для организаций предусмотрена гибкая система скидок
+                    </div>
+                </div>                    
+
+                <div class="col">
+                    <div class="want">Хотите сэкономить до 30%? Получить<br> надежного партнера,  избавиться от <br>головных болей и всегда знать о том, что<br> ваши сотрудники будут иметь<br> необходимые документы на руках?</div>
+                    <div class="offer">Читать индивидуальное предложение</div>
+                </div>
+            </div>
         </div>
-    
-    </body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="js/megamenu.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
 </html>
