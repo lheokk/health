2211d133113fbaf5dfba706db6e8259697ec2cc0
anovicenko74 <60508206+anovicenko74@users.noreply.github.com>
2020-10-03 23:46:33 +0200

    03.10.20 result

---
+++ tpl/main_template2.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=<?= $g_config['charset'] ?>" />
+
+        <title><?= $g_title ?></title>
+        <?php if (!empty($g_description)): ?>
+            <meta name="description" content="<?= $g_description ?>" />
+        <?php endif; ?>
+        <?php if (!empty($g_keywords)): ?>
+            <meta name="keywords" content="<?= $g_keywords ?>" />
+        <?php endif; ?>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="icon" href="<?= Root('favicon.ico') ?>" type="image/x-icon" />
+        <link rel="shortcut icon" href="<?= Root('favicon.ico') ?>" type="image/x-icon" />
+
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/normalize.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/dev/funcs.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/main.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/style.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= Root('i/css/megamenu.css') ?>" />
+       
+        <!-- карусель тут подкюлчатеся -->
+        <link rel="stylesheet" href="<?= Root('i/css/owl.carousel.min.css')?>">
+              
+        <link rel="stylesheet" href="<?= Root ('i/css/owl.theme.default.min.css')?>">
+       
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="<?= Root ('i/js/owl.carousel.min.js') ?>"></script>
+       
+        <script type="text/javascript" src="<?= Root('i/js/main.js') ?>"></script>
+       
+        <!-- extraPacker -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+    <body>
+        <style> .full {width: 100%; height: auto}</style>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="star">
+                        <div class='rusmed'>
+                            РУСМЕДЗДРАВ
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col">
+                    <div class="menu-container">
+                        <div class="menu">
+                            <ul>
+                                <li>О центре</li>
+                                <li>Услуги</li>
+                                <li>Акции</li>
+                                <li>Партнёрство</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col">
+                    <div class="menu-container ">
+                        <div class="menu ">
+                            <ul>
+                                <li>Карьера</li>
+                                <li>Блог</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <div class='full blue-line'> 
+            <div class="container ">
+                <div class="row">
+                    <div class="col">
+                        <div style="padding: 25px 0;">
+                            COVID-19. Тестирование на коронавирус с выездом на дом и предприятие. Читать подробнее
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="med">
+                </div>
+            </div>
+        </div>
+            </div>
+        <div class='container'>    
+            <div class='row'>
+                <div class='col-lg-4 col-md-4'>
+                    <div class='bell'>
+                        Заказать обратный звонок
+                    </div>
+                </div>
+                <div class='col-lg-8 col-md-8'>
+                    ООО «РУСМЕДЗДРАВ» предоставляет полный <br> пакет услуг согласно Приказу Министерства <br> здравоохранения и социального развития РФ <br> от 12.04.2011№ 302 н «Об утверждении перечней <br> вредных и (или) опасных производственных <br> факторов, при выполнении которых проводятся <br> обязательные предварительные и периодические <br> осмотры (обследования)»
+                </div>
+            </div>
+        </div>
+        <div class='bg-F7'>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="list-img"></div>
+                                </div>
+                                <div class="col">
+                                    <div class="med-book">Медицинские справки </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="garant">
+                                        Мы обладаем медицинской лицензией МинЗдрава РФ и предоставляет только официальные документы...
+                                    </div>
+                                    <div class="read"> Читать полностью...</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=""></div>
+                        <div class="doctor-img"></div>
+                        <div class="low"> Дешевле, чем у конкурентов</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class=""></div>
+                        <div class="doctor-img"></div>
+                        <div class="low"> Дешевле, чем у конкурентов</div>
+                    </div>
+                    <div class="col">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="list-img"></div>
+                                </div>
+                                <div class="col">
+                                    <div class="med-book">Медицинские справки</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="garant">
+                                        Мы обладаем медицинской лицензией МинЗдрава РФ и предоставляет только официальные документы...
+                                    </div>
+                                    <div class="read"> Читать полностью...</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="star2"></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="success">Наш успех заключается не только в <br> качестве и в максимальном комфорте для <br> организаций и частных лиц. В первую <br> очень - это приятные цены.</div>
+                    <div class="prices"> Развернуть цены на услуги </div>
+                </div> 
+            </div> 
+        </div> 
+
+        <div class="bg-v">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="online"> Оставить онлайн заявку </div>
+                        <div class="free">Чтобы получить бесплатную консультацию <br> физическому или юридическому лицу, <br> необходимо заполнить форму или обратиться <br>к нашим специалистам по телефону <br>
+                            + 7 (495) 666-0-00. </div>
+                    </div>
+
+
+                    <div class="col">
+
+                        <div class="form form-1">
+                            <input type="text" value="" name="first_name"  placeholder="Имя" class="form-input" />
+                        </div>
+
+                        <div class="form ">
+                            <input type="text" value="" name="second_name" placeholder="Фамилия" class="form-input" />
+                        </div>
+
+                        <div class="form">
+                            <input type="text" value="" name="email"  placeholder="Номер телефона" class="form-input" />
+                        </div>
+
+                        <div class="form ">
+                            <input type="text" value="" name="title"  placeholder="Заголовок" class="form-input" />
+                        </div>
+
+                        <div class="form">
+                            <input type="text" value="" name="Company"  placeholder="Компания" class="form-input" />
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div class='up'></div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="sales">
+                        Для организаций предусмотрена гибкая система скидок
+                    </div>
+                </div>                    
+
+                <div class="col">
+                    <div class="want">Хотите сэкономить до 30%? Получить<br> надежного партнера,  избавиться от <br>головных болей и всегда знать о том, что<br> ваши сотрудники будут иметь<br> необходимые документы на руках?</div>
+                    <div class="offer">Читать индивидуальное предложение</div>
+                </div>
+            </div>
+        </div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="us">
+            С нами работали:
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+         <div class="owl-carousel owl-theme">
+            <div class="item" style="width:285px"><img src="/i/image/metropol.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/okay.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/hotel.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/globus.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/cosmic.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/moscow.jpg"></div>
+<!--        <div class="item" style="width:285px"><img src="/i/image/ukraine.png"></div>-->
+            <div class="item" style="width:285px"><img src="/i/image/procons.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/line.jpg"></div>
+            <div class="item" style="width:285px"><img src="/i/image/work.jpg"></div>
+        </div>
+        </div>
+    </div>
+</div>        <script>
+            $('.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:4 })
+      </script>
+      
+      
+    <div class="bg-F7">
+       <div class="container">
+            <div class="row">
+                <div class="col">
+    <div class="owl-carousel owl-theme">
+    
+       <div class="pr"> 
+    <div class="item"><h4>
+        <div class="procent"></div>
+        <div class="v_text_16">Акция</div>
+        <div class="rectangle"></div>
+        <div class=""></div></h4>
+    </div></div>
+        
+        <div class="pr"> 
+         <div class="item"><h4>
+        <div class="procent"></div>
+        <div class="v_text_16">Акция</div>
+        <div class="rectangle"></div>
+        <div class=""></div></h4>
+    </div></div>
+        
+    
+ 
+        
+    
+    </div>
+                 </div>
+            </div>
+        </div>
+    </div>  
+    
+        <script>
+           $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+      </script>
+    
+      </div>    
+       
+      <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="cookie">
+                    Cookie/обработка данных
+                    </div>
+                </div>
+                <div class="col-md-1">
+                    <div class="star star_"></div>
+                </div>
+                <div class="col-md-8">
+                    <div class="RUS">РУСМЕДЗДРАВ</div>
+                </div>
+            </div>
+          
+          <div class="container">
+              <div class="row">
+                  <div class="col">
+                      <div class="star_big"></div>
+                    </div>
+                </div>  
+                  <div class="row">
+                      <div class="col">
+                           <div class="index-text"><h1>МЕДИЦИНСКИЙ ЦЕНТР, МЕДИЦИНСКИЕ СПРАВКИ, МЕДИЦИНСКИЕ ОСМОТРЫ</h1></div>
+                      </div>
+                  </div>
+                     
+                  <div class="row">
+                      <div class="col">
+                           <div class="index-text"><h2>1. МЕДИЦИНСКИЙ ЦЕНТР РУСМЕДЗДРАВ</h2></div>
+                      </div>
+                  </div>
+                  
+                <div class="row">
+                      <div class="col">
+                           <div class="index-text"><p>РУСМЕДЗДРАВ - компания, которая успешно зарекомендовавшая себя на рынке предоставления медицинских услуг. Мы предлагаем услуги по проведению предварительных и периодических медицинских осмотров для сотрудников вашей организации, а так же все виды справок, сертификаты нарколога и психиатра. 
+                               </p><p>Согласно действующему законодательству Российской Федерации, отдельные работники, поступающие на работу, обязаны пройти предварительный медицинский осмотр. Лиц, не достигших возраста 21 года и граждан, занятых на работах с вредными и/или опасными условиями труда, предусмотрены ежегодные периодические медицинские осмотры (для остальных профессий в возрасте старше 21 года - не реже, чем 1 раз в 2 года). <p></div>
+                      </div>
+                  </div>
+                   
+                 
+                <div class="row">
+                      <div class="col">
+                           <div class="index-text"><h3></h3></div>
+                      </div>
+                  </div>
+              
+          </div>
+            <div class="row">
+                <div class="col">
+                    <ul class="footer">
+                        <li><a href="#">Справка для водителей</a></li>
+                        <li><a href="#">Справка на оружие</a></li>
+                        <li><a href="#">Справка для бассейна и фитнеса</a></li>
+                        <li><a href="#">Справка в ВУЗ</a></li>
+                        <li><a href="#">Справка в для госслужбы</a></li>
+                      
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="footer">
+                        <li><a href="#">Профосмотры</a></li>
+                        <li><a href="#">Медкнижки</a></li>
+                        <li><a href="#">Выездные профосмотры</a></li>
+                        <li><a href="#">Психиатричесое свидетельство</a></li>
+                        <li><a href="#">Справки для госслужбы</a></li>
+                       
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="footer">
+                        
+                        <li><a href="#">Отзывы</a></li>
+                        <li><a href="#">Документы</a></li>
+                        <li><a href="#">Партнеры</a></li>
+                        <li><a href="#">Структура</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>   
+                </div>
+            </div>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script src="<?= Root('i/js/megamenu.js') ?>"></script>
+
+    </body>
+</html>
