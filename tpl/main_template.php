cf295d2aa561575552521d880193966bc6be3b1f
anovicenko74 <60508206+anovicenko74@users.noreply.github.com>
2020-10-05 16:16:39 +0200

    заллилилаилавл

--- tpl/main_template.php
+++ tpl/main_template.php
@@ -103,12 +103,17 @@
                     ООО «РУСМЕДЗДРАВ» предоставляет полный <br> пакет услуг согласно Приказу Министерства <br> здравоохранения и социального развития РФ <br> от 12.04.2011№ 302 н «Об утверждении перечней <br> вредных и (или) опасных производственных <br> факторов, при выполнении которых проводятся <br> обязательные предварительные и периодические <br> осмотры (обследования)»
                 </div>
             </div>
+        </div><link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet" />
+        <div class="row flex-column-reverse flex-lg-row">
+            <div class="col-md-12 col-lg-6 col-xl-7">A</div>
+            <div class="col-md-12 col-lg-6 col-xl-5">B</div>
         </div>
         <div class='bg-F7'>
             <div class="space50"></div>
             <div class="container">
                 <div class="row">
-                    <div class="col">
+                    <div class="row flex-column-reverse flex-lg-row">
+                    <div class="col-md-6">
                         <div class="container">
                             <div class="row">
                                 <div class="col-5">
@@ -128,12 +133,12 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col">
+                    <div class="col-md-6">
                         <div class=""></div>
                         <div class="doctor-img"></div>
                         <div class="low"> Дешевле, чем у конкурентов</div>
                     </div>
-                </div>
+                </div></div>
             </div>
             <div class="space50"></div>
             <div class="container">
