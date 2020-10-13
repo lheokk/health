758f0f7cbe4efbf62a747c5ed4f13092558a849d
lheokk <lheokk@gmail.com>
2020-10-13 11:10:59 +0200

    da

--- tpl/modal.php
+++ tpl/modal.php
@@ -4,5 +4,7 @@
         <a href="#close2" title="Закрыть" class="close2">закрыть</a>
         <h2>Введите имя и телефон</h2>
         <p> <input type="text" value="" name="first_name"  placeholder="Представьтесь, пожалуйста" class="modal-input" /></p>
-<p> <input type="text" value="" name="first_name"  placeholder="Контактный телефон" class="modal-input" /></p>    </div>
+        <p> <input type="text" value="" name="first_name"  placeholder="Контактный телефон" class="modal-input" /></p> 
+        <p class="sent"> Нажимая кнопку Отправить<br> Вы подтверждаете согласие на обработку персональных данных</p>
+    </div>
 </div>
