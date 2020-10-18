c95c9cf4ca9be15fd567ad29df6c26e8cd255a9c
anovicenko74 <60508206+anovicenko74@users.noreply.github.com>
2020-10-18 15:31:20 +0200

    fd

--- tpl/page_button.php
+++ tpl/page_button.php
@@ -15,8 +15,8 @@
     <div style='padding-top: 50px;'>
         <a href="#close2" class="close2">закрыть</a>
         <h2>Введите имя и телефон</h2>
-        <p> <input type="text" value="" name="first_name"  placeholder="Представьтесь, пожалуйста" class="modal-input" /></p>
-        <p> <input type="text" value="" name="first_name"  placeholder="Контактный телефон" class="modal-input" /></p> 
+        <p> <input type="text" value="" name="first_name"  placeholder="Представьтесь, пожалуйста" class="modal-input" style=" height: 56px;" /></p>
+        <p> <input type="text" value="" name="first_name"  placeholder="Контактный телефон" class="modal-input" style=" height: 56px;" /></p> 
         <p class="sent"> Нажимая кнопку Отправить<br> Вы подтверждаете согласие на обработку персональных данных</p>
         <a href="javascript:void(0);" class="btn">Отправить</a>
     </div>
